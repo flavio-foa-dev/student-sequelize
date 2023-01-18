35728f6232763b4d172b193cf456e8db7daabf04
Flavio Andrade <foa12381@gmail.com>
2023-01-18 20:53:18 -0300

    addMYSQL

---
+++ exemple.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+ const Pessoas = sequelize.define('Pessoas', {
+   nome: DataTypes.STRING,
+   ativo: DataTypes.BOOLEAN,
+   email: DataTypes.STRING,
+   role: DataTypes.STRING
+ }, {});
+ Pessoas.associate = function(models) {
+   // associations can be defined here
+ };
+ return Pessoas;
+};
+
+
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+ class Pessoas extends Model {
+   static associate(models) {
+     // associations can be defined here
+   }
+ }
+
+ Pessoas.init({
+   nome: DataTypes.STRING,
+   ativo: DataTypes.BOOLEAN,
+   email: DataTypes.STRING,
+   role: DataTypes.STRING
+ }, {
+   sequelize,
+   modelName: 'Pessoas',
+ });
+ return Pessoas;
+};
