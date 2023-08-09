3b4132ca2dc31822ac2b85c6bdab52ab7105c011
Flavio Andrade <foa12381@gmail.com>
2023-08-09 09:39:17 -0300

    f

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const usersRouter = require('./src/routes/usersRouter');
-
 const app = express();
-
 app.use(express.json());
-
-
-
 app.use(usersRouter)
-
 app.listen(3000, () => console.log('listening on port 3000'));
-
 module.exports = app;
